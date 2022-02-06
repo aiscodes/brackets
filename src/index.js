7a48baba55d195684eddfff7325e4fff8a3efb78
aiscodes <aigerim6769@gmail.com>
2022-02-06 17:20:15 +0600

    task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj = {}
+  bracketsConfig.map(el => obj[el[0]] = el[1])
+
+  const all = []
+
+  for (let one of str) {
+    const lastOne = all[all.length - 1]
+    if (one in obj) { 
+      obj[one] === one && one === lastOne ? all.pop() : all.push(one)
+    } else {
+      const last = obj[lastOne]
+      if (last === one) {
+        all.pop()
+      } else {
+        return false
+      }
+    }
+  }
+
+  return all.length === 0
 }
